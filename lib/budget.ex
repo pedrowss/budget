9113ef6a399d3b1d8071b491d9b8bb0d69d0d1f4
Pedro Siqueira <pedrowss@gmail.com>
2017-09-30 20:28:30 -0300

    Add nimble csv a third-party dependency

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,5 +1,12 @@
 defmodule Budget do
+  alias NimbleCSV.RFC4180, as: CSV
+
   def list_transactions do
-    File.read("lib/transactions.csv")
+    File.read!("lib/transactions.csv")
+    |> parser
+  end
+
+  defp parser(string) do
+    CSV.parse_string(string)
   end
 end
--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Budget.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:nimble_csv, "~> 0.2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
